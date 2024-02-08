8efab463a537675933f95f363e27be157671d05e
betty29tesfaye <144184498+betty29tesfaye@users.noreply.github.com>
2024-02-08 17:04:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,13 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import streamlit as st
 from PIL import Image
 
-file_path = '/content/drive/MyDrive/Colab Notebooks/Comparing_apples_and_oranges_.pdf'
-loaded_file = PyPDFLoader(file_path=file_path)
-
 from langchain.chains.summarize import load_summarize_chain
 from langchain.chat_models import ChatOpenAI
 
 def temporarly_save_uploaded_file(uploadedfile):
      with open(uploadedfile.name,"wb") as f:
          f.write(uploadedfile.getbuffer())
-
+          
 text_splitter = RecursiveCharacterTextSplitter(
    chunk_size=1000,
    chunk_overlap=100,
@@ -44,13 +41,18 @@ add_background_image('bgi.png')
 st.markdown(f'<span style="background-color:#DFF2FF;color:#0F52BA;font-family:book-antiqua;font-size:24px;">AI App For Text Summarizing</span>', unsafe_allow_html=True)
 uploaded_file = st.file_uploader(' ')
 
+def temporarly_save_uploaded_file(uploadedfile):
+     with open(uploadedfile.name,"wb") as f:
+         f.write(uploadedfile.getbuffer())
+          
+temporarly_save_uploaded_file(uploaded_file.name)
+
+loaded_file = PyPDFLoader(file_path=file_path)
 data_chunks = loaded_file.load_and_split(text_splitter=text_splitter)
 
 #Defining the large language Model
 
-
 llm = ChatOpenAI()
-
 
 chain = load_summarize_chain(
    llm=llm,
