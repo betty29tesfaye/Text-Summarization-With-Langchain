23f650b593ced8e352bc1b3b8a2bbb09fffa7b07
betty29tesfaye <144184498+betty29tesfaye@users.noreply.github.com>
2024-02-09 14:01:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,7 @@ if uploaded_file is not None:
         llm=llm,
         chain_type='refine'
         )
-   st.write(chain.run(data_chunks))
+    st.markdown(f'<p style="background-color:#b3cee5;color:#414C6B;">Here is the summary of the file you uploaded</p>', unsafe_allow_html=True) 
+      
+   st.markdown(f'<p style="background-color:#b3cee5;color:#414C6B;">{chain.run(data_chunks)}</p>', unsafe_allow_html=True) 
+
